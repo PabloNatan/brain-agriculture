2590e9a6f15d778b8bb21c951327d2b92b3da38b
Pablo Santos <pablonatan.d.r.s@gmail.com>
2025-05-27 21:34:28 -0300

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomConfigService } from './custom-config/custom-config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(CustomConfigService);
+  app.setGlobalPrefix('/products-api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Brain Agriculture API')
+    .setDescription('API to manage the registration of rural producers')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  const port = configService.port;
+
+  await app.listen(port ?? 3000);
 }
 bootstrap();
